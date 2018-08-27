398ca09561bdf6ca2e9b143fd5aac92d0276f281
824126167@qq.com <78963214l>
2018-08-27 17:35:32 +0800

    类似mybatisgenetor生成类,可以修改成类似自己公司的模板

---
+++ src/main/java/com/together/knowledge/base/generatorcode/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * 生成模板类,类似mybatis-generator，可以根据fremaker生成适合于本公司的模板，可以打成jar包，和mybatis-generator一起使用
+ * @author liuy
+ */
+package com.together.knowledge.base.generatorcode;
--- src/main/java/com/together/knowledge/base/invoke/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * 动态代理
- * @author liuy
- */
-package com.together.knowledge.base.invoke;
