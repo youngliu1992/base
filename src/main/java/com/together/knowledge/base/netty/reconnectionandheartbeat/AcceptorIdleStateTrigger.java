a0c3ef0e0e613bca8d911fabe249aaaf19e6e407
824126167@qq.com <78963214l>
2018-09-05 18:18:43 +0800

    添加netty模块的重连

---
+++ src/main/java/com/together/knowledge/base/netty/reconnectionandheartbeat/AcceptorIdleStateTrigger.java
@@ -0,0 +1,31 @@
+package com.together.knowledge.base.netty.reconnectionandheartbeat;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.timeout.IdleState;
+import io.netty.handler.timeout.IdleStateEvent;
+import io.netty.util.CharsetUtil;
+
+
+@ChannelHandler.Sharable
+public class AcceptorIdleStateTrigger extends ChannelInboundHandlerAdapter {
+
+    private static final ByteBuf HEARTBEAT_SEQUENCE = Unpooled.unreleasableBuffer(Unpooled.copiedBuffer("Heartbeat",
+            CharsetUtil.UTF_8));
+
+    @Override
+    public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+        if (evt instanceof IdleStateEvent) {
+            IdleState state = ((IdleStateEvent) evt).state();
+            if (state == IdleState.READER_IDLE) {
+                //throw new Exception("idle exception");
+                ctx.writeAndFlush(HEARTBEAT_SEQUENCE.duplicate());
+            }
+        } else {
+            super.userEventTriggered(ctx, evt);
+        }
+    }
+}
---
+++ src/main/java/com/together/knowledge/base/netty/reconnectionandheartbeat/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * netty 实例  心跳重连
+ * @author liuy
+ */
+package com.together.knowledge.base.netty.reconnectionandheartbeat;
